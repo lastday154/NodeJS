1282fd66790e2bc24cb07c7a2cf204209965242f
Steve Tuan <vothuong197@gmail.com>
2016-12-18 23:07:57 +0800

    basic home page with boostrap

--- basic-express-website/app.js
+++ basic-express-website/app.js
@@ -5,11 +5,20 @@ var nodemailer = require('nodemailer');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',(req, res)=> {
-	res.send('<h1> Hello, Steve </h1>');
+app.get('/', function(req, res){
+	res.render('index',{title: 'Welcome'});
 });
+
+app.get('/about', function(req, res){
+	res.render('about');
+});
+
 app.listen(3000);
-console.log('Server is running on port 3000 ....');
+console.log('Server is running on port 3000...');
