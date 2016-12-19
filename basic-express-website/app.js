3878621087a7e65f0a347a2dc011c630e70a51ea
Steve Tuan <vothuong197@gmail.com>
2016-12-19 08:27:03 +0800

    complete basic express website

--- basic-express-website/app.js
+++ basic-express-website/app.js
@@ -13,12 +13,45 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-	res.render('index',{title: 'Welcome'});
+	res.render('index',{title: 'Computer Not Working'});
 });
 
 app.get('/about', function(req, res){
 	res.render('about');
 });
 
+app.get('/contact', (req, res) => {
+	res.render('contact');
+});
+
+app.post('/contact/send', (req, res) => {
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'hmtuan197@gmail.com',
+			pass: 'placedwq'
+		}
+	});
+
+	var mailOptions = {
+		from: 'Steve Tuan <hmtuan197@gmail.com>',
+		to: 'vothuong197@gmail.com',
+		subject: 'Website submission',
+		text: 'You have a submission with the details... Name ' + req.body.name + ' Email ' + req.body.email + ' Message ' + req.body.message,
+		html: '<p>You have a submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message Sent: '+info.response);
+			res.redirect('/');
+		}
+	});
+
+
+});
 app.listen(3000);
 console.log('Server is running on port 3000...');
