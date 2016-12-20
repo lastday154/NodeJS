af6ae40655a007ba243796cc0deee6b320153d4e
Steve Tuan <vothuong197@gmail.com>
2016-12-20 10:59:25 +0800

    create show route to get post by id

--- node-blog-system/routes/posts.js
+++ node-blog-system/routes/posts.js
@@ -5,6 +5,17 @@ var upload = multer({ dest: './public/images' })
 var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
+router.get('/show/:id', function(req, res, next) {
+	var posts = db.get('posts');
+
+	posts.findById(req.params.id, function(err, post){
+		res.render('show', {
+  			'post': post
+  		});
+	});
+});
+
+
 router.get('/add', function(req, res, next) {
 	var categories = db.get('categories');
 
